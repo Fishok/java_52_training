ecdaff7f4ec8d0b7ba3a795fffe652cdc2e17610
Alex <sashafish93@gmial.com>
2024-05-07 19:25:22 +0300

    Init commit

---
+++ src/test/java/telran/webinar/SimpleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.webinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
